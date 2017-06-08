649ac74a11e4b2d1ab43b0e6807969b3859a40c2
David Caine <dc9884@users.noreply.github.com>
2017-06-07 22:24:55 -0700

    add validations for blank start and end times, and times in the past

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -20,14 +20,46 @@ class Permit < ApplicationRecord
     def location_not_already_booked
       same_location_permits = self.location.permits
 
-      start_time_overlaps = same_location_permits.where(:booked_time => self.booked_time..self.booked_end)
+      if :booked_time || :booked_end == nil
 
-      end_time_overlaps = same_location_permits.where(:booked_end => self.booked_time..self.booked_end)
+      else
 
-      if start_time_overlaps.any? || end_time_overlaps.any?
-        errors.add(:location_id, "The selected location is already booked during that time.")
+        start_time_overlaps = same_location_permits.where(:booked_time => self.booked_time..self.booked_end)
+
+        end_time_overlaps = same_location_permits.where(:booked_end => self.booked_time..self.booked_end)
+
+        if start_time_overlaps.any? || end_time_overlaps.any?
+          errors.add(:location_id, "The selected location is already booked during that time.")
+        end
       end
     end
+
+  validate :booked_time_cannot_be_in_the_past
+
+    def booked_time_cannot_be_in_the_past
+
+      if booked_time.present? && booked_time < Date.today
+        errors.add(:booked_time, "cannot be in the past.")
+      end
+    end
+
+    validate :booked_end_cannot_be_in_the_past
+
+      def booked_end_cannot_be_in_the_past
+
+        if booked_end.present? && booked_end < Date.today
+          errors.add(:booked_end, "cannot be in the past.")
+        end
+      end
+
+      validate :booked_end_cannot_be_before_booked_time
+
+        def booked_end_cannot_be_before_booked_time
+
+          if booked_end.present? && booked_end < booked_time
+            errors.add(:booked_end, "cannot be before start time.")
+          end
+        end
 
   belongs_to :movie
   belongs_to :user
